b0784178b2955df6a0272d87598460b64ed97b2d
maxmoniot <max.gayet@gmail.com>
2025-12-19 14:58:32 +0100

    1.0

---
+++ test.ts
@@ -0,0 +1,12 @@
+robot.elecfreaksCuteBot.start()
+//robot.yahboomTinyBit.start(
+//robot.elecfreaksCuteBotPro.start()
+//robot.keyStudioMiniSmartRobot.start()
+//robot.setMotorDrift(6)
+//robot.dfRobotMaqueen.start()
+//robot.dfRobotMaqueenPlusV2.start()
+//robot.kittenbotMiniLFR.start()
+//robot.inksmithK8.start()
+//robot.startCompactRadio()
+//robot.startCalibrationButtons()
+robot.test.startTestMode()
